65eaec7f42b4eb2f6b1efc33ff67d445912ca9e0
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-04-15 14:39:24 +0100

    Add wallet connect with CDN CSS

---
+++ app/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/@aptos-labs/wallet-adapter-ant-design@latest/dist/index.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import type { Metadata } from "next";
 import "./globals.css";
 import { AptosWalletAdapterProvider } from "@aptos-labs/wallet-adapter-react";
 import { PetraWallet } from "petra-plugin-wallet-adapter";
